f6e75d9183ebbfab4506fa1f57f9747dbe35d0b9
vincent <120380476+vincentfinalversion@users.noreply.github.com>
2026-09-15 19:25:28 +0800

    feat: create FilterPanel for HelperPanel

--- frontend/src/components/HelperPanel/HelperPanel.tsx
+++ frontend/src/components/HelperPanel/HelperPanel.tsx
@@ -5,9 +5,13 @@ import './HelperPanel.css';
 function SearchPanel(){
   return (
     <div className='search-panel-container'>
+      <label className='search-panel-label'>
+        Search
+      </label>
+
       <TextBox 
-        label='Search'
-        placeholder='Find a specific word'
+        label='Find a specific word'
+        placeholder='Enter a word'
       />
 
       <Button>
@@ -18,7 +22,35 @@ function SearchPanel(){
 }
 
 function FilterPanel() {
-  return 0;
+  return (
+    <div className='filter-panel-container'>
+      <label className='filter-panel-label'>
+        Filters
+      </label>
+
+      <div className='filter-textbox-container'>
+        <TextBox
+          label='Filter by starting letter'
+          placeholder='Enter a letter'
+        />
+        <TextBox
+          label='Filter by character count'
+          placeholder='Enter character amount'
+        />
+      </div>
+
+      <div className='filter-button-container'>
+        <Button
+          variant='destructive'
+        >
+          Clear Filters
+        </Button>
+        <Button>
+          Apply Filters
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 function HelperPanel() {
